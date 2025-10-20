c3e83285cfa5c36c8da30538c50f798c953dbe31
abysoll-hg <slickspam4u@gmail.com>
2025-10-20 09:20:53 -0700

    Create Assignment102025.java
    Added Java file so it works

---
+++ Assignment102025.java
@@ -0,0 +1,23 @@
+public class Assignment102025
+{
+   public static void main(String[] Args)
+   {
+      asciiArt();
+      System.out.println("This is an axe");
+   }
+   public static void asciiArt()
+   {
+System.out.println("          A\n" +
+         "         /!\\\\\n" +
+         "        / ! \\\\\n" +
+         "/\\     )___(\n" +
+         "(  `.____(_)_________\n" +
+         "|           __..--\"\"\"\n" +
+         "(       _.-|\n" +
+         " \\\\    ,' | |\n" +
+         "  \\\\  /   | |\n" +
+         "   \\\\(    | |\n" +
+         "    `     | |\n" +
+         "          | |\n");
+   }
+}
